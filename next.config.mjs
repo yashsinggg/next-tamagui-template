88519a730e1189b409c5db557a0f257b1537aecc
yashsinggg <yoyash2003@gmail.com>
2024-09-03 18:24:46 +0530

    added all the packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const { withTamagui } = require("@tamagui/next-plugin");
+module.exports = function (name, { defaultConfig }) {
+  let config = {
+    ...defaultConfig,
 
-export default nextConfig;
+    // ...your configuration
+  };
+  const tamaguiPlugin = withTamagui({
+    config: "./tamagui.config.ts",
+
+    components: ["tamagui"],
+
+    appDir: true,
+
+    outputCSS:
+      process.env.NODE_ENV === "production" ? "./public/tamagui.css" : null,
+    disableExtraction: process.env.NODE_ENV === "development",
+  });
+  return {
+    ...config,
+
+    ...tamaguiPlugin(config),
+  };
+};
